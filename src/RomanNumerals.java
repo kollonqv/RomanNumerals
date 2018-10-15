35a095a5dd9a9e5c6a1836530075fdbba6239eb0
kalle <kalle@kalle-UX410UAK>
2018-10-15 15:19:48 +0300

    Assignment Finished

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,8 +1,27 @@
-
 public class RomanNumerals {
+	
+	public final String[] ROMAN_NUMERALS = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+	public final int[] INTS_FROM_ROMAN = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+	
 	public int convertToInteger(String romanNum) {
-		// To be Implemented
-		return 0;
 		
+		String[] nums = romanNum.split("");
+		int result = 0;
+		int[] ints = new int[nums.length];
+
+		int x = 0;
+		for (int i = 0; i < INTS_FROM_ROMAN.length; i++ ) {
+		    while (romanNum.indexOf(ROMAN_NUMERALS[i]) == 0) {
+		        ints[x] = INTS_FROM_ROMAN[i];
+		        romanNum = romanNum.substring(ROMAN_NUMERALS[i].length());
+		        x++;
+		    }
+		}
+		
+		for(int i = 0; i < ints.length; i++) {
+			result = result + ints[i];
+		}
+		
+		return result;
 	}
 }
--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -5,8 +5,165 @@ import org.junit.Test;
 public class TestRomanNumerals {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void convertToInteger_II_is_2() {
+		
+		RomanNumerals romanNumerals = new RomanNumerals();
+		
+		int result = romanNumerals.convertToInteger("II");
+		
+		assertEquals("2", 2, result);
 	}
-
+	
+	@Test
+	public void convertToInteger_Vto5() {
+		
+		RomanNumerals romanNumerals = new RomanNumerals();
+		
+		int result = romanNumerals.convertToInteger("V");
+		
+		assertEquals("V", 5, result);
+	}
+	
+	@Test
+	public void convertToInteger_Xto10() {
+		
+		RomanNumerals romanNumerals = new RomanNumerals();
+		
+		int result = romanNumerals.convertToInteger("X");
+		
+		assertEquals("X", 10, result);
+	}
+	
+	@Test
+	public void convertToInteger_Lto50() {
+		
+		RomanNumerals romanNumerals = new RomanNumerals();
+		
+		int result = romanNumerals.convertToInteger("L");
+		
+		assertEquals("L", 50, result);
+	}
+	
+	@Test
+	public void convertToInteger_Cto100() {
+		
+		RomanNumerals romanNumerals = new RomanNumerals();
+		
+		int result = romanNumerals.convertToInteger("C");
+		
+		assertEquals("C", 100, result);
+	}
+	
+	@Test
+	public void convertToInteger_Dto500() {
+		
+		RomanNumerals romanNumerals = new RomanNumerals();
+		
+		int result = romanNumerals.convertToInteger("D");
+		
+		assertEquals("D", 500, result);
+	}
+	
+	@Test
+	public void convertToInteger_Mto1000() {
+		
+		RomanNumerals romanNumerals = new RomanNumerals();
+		
+		int result = romanNumerals.convertToInteger("M");
+		
+		assertEquals("M", 1000, result);
+	}
+	
+	@Test
+	public void convertToInteger_VIIIto8() {
+		
+		RomanNumerals romanNumerals = new RomanNumerals();
+		
+		int result = romanNumerals.convertToInteger("VIII");
+		
+		assertEquals("VIII", 8, result);
+	}
+	
+	@Test
+	public void convertToInteger_DCCCto800() {
+		
+		RomanNumerals romanNumerals = new RomanNumerals();
+		
+		int result = romanNumerals.convertToInteger("DCCC");
+		
+		assertEquals("DCCC", 800, result);
+	}
+	 
+	@Test
+	public void convertToInteger_IVto4() {
+		
+		RomanNumerals romanNumerals = new RomanNumerals();
+		
+		int result = romanNumerals.convertToInteger("IV");
+		
+		assertEquals("IV", 4, result);
+	}
+	
+	@Test
+	public void convertToInteger_IXto9() {
+		
+		RomanNumerals romanNumerals = new RomanNumerals();
+		
+		int result = romanNumerals.convertToInteger("IX");
+		
+		assertEquals("IX", 9, result);
+	}
+	
+	@Test
+	public void convertToInteger_XLto40() {
+		
+		RomanNumerals romanNumerals = new RomanNumerals();
+		
+		int result = romanNumerals.convertToInteger("XL");
+		
+		assertEquals("XL", 40, result);
+	}
+	
+	@Test
+	public void convertToInteger_XCto90() {
+		
+		RomanNumerals romanNumerals = new RomanNumerals();
+		
+		int result = romanNumerals.convertToInteger("XC");
+		
+		assertEquals("XC", 90, result);
+	}
+	
+	@Test
+	public void convertToInteger_CDto400() {
+		
+		RomanNumerals romanNumerals = new RomanNumerals();
+		
+		int result = romanNumerals.convertToInteger("CD");
+		
+		assertEquals("CD", 400, result);
+	}
+	
+	@Test
+	public void convertToInteger_CMto900() {
+		
+		RomanNumerals romanNumerals = new RomanNumerals();
+		
+		int result = romanNumerals.convertToInteger("CM");
+		
+		assertEquals("CM", 900, result);
+	}
+	 
+	
+	
+	@Test
+	public void convertToInteger_MCMLXXXIVto1984 () {
+		
+		RomanNumerals romanNumerals = new RomanNumerals();
+		
+		int result = romanNumerals.convertToInteger("MCMLXXXIV");
+		
+		assertEquals("MCMLXXXIV", 1984, result);
+	}
+	
 }
